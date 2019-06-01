dbf09b09712fa883ba750867e908ece36c762f87
StrangeBoyC <1915554670@qq.com>
2019-06-01 16:36:12 +0800

    6.1更新

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
+
 window.onload = function () {
+
+
     let home = document.getElementById("home");
 
-    home.onmousemove = function () {
+    home.onmouseenter = function () {
         home.style.color = "red";
     }
     home.onmouseleave = function () {
@@ -14,30 +17,29 @@ window.onload = function () {
     let radioLi = radio.getElementsByTagName("li");
     let leftBtn = document.getElementsByClassName("left-btn")[0];
     let rightBtn = document.getElementsByClassName("right-btn")[0];
+    let width = turnsLi[0].offsetWidth;
+    let flag = true;
 
-
-    for(let i=0;i<radioLi.length;i++){
+/*    for(let i=0;i<radioLi.length;i++){
         radioLi[i].onclick = function () {
             for(let j=0;j<radioLi.length;j++){
-                turnsLi[j].style.zIndex="1";
+                // turnsLi[j].style.zIndex="1";
+                animate(turnsLi[j],{opacity:0},500);
                 radioLi[j].style.background= "#fff";
             }
-            turnsLi[i].style.zIndex="999";
+            // turnsLi[i].style.zIndex="999";
+            animate(turnsLi[i],{opacity:1},500,);
+
             radio.style.zIndex="999";
             radioLi[i].style.background= "#12b7de";
             leftBtn.style.zIndex="999";
             rightBtn.style.zIndex="999";
             index=i;
         }
-/*        radioLi[i].onmouseenter=function () {
-            for(let j=0;j<radioLi.length;j++){
-                radioLi[j].style.background= "#fff";
-            }
-            radioLi[i].style.background= "#12b7de";
-        }*/
+    }*/
 
-    }
-    let index=0;
+    //透明过渡
+    /*let index=0;
     leftBtn.onclick=function(){
         console.log(index);
         index--;
@@ -45,10 +47,12 @@ window.onload = function () {
             index=turnsLi.length-1;
         }
         for(let j=0;j<turnsLi.length;j++){
-            turnsLi[j].style.zIndex="1";
+            // turnsLi[j].style.zIndex="1";
+            animate(turnsLi[j],{opacity:0},500);
             radioLi[j].style.background= "#fff";
         }
-        turnsLi[index].style.zIndex="999";
+        // turnsLi[index].style.zIndex="999";
+        animate(turnsLi[index],{opacity:1},500,);
         radio.style.zIndex="999";
         radioLi[index].style.background= "#12b7de";
         leftBtn.style.zIndex="999";
@@ -60,15 +64,92 @@ window.onload = function () {
             index=0;
         }
         for(let j=0;j<turnsLi.length;j++){
-            turnsLi[j].style.zIndex="1";
+            // turnsLi[j].style.zIndex="1";
+            animate(turnsLi[j],{opacity:0},500);
             radioLi[j].style.background= "#fff";
         }
-        turnsLi[index].style.zIndex="999";
+        // turnsLi[index].style.zIndex="999";
+        animate(turnsLi[index],{opacity:1},500,);
         radio.style.zIndex="999";
         radioLi[index].style.background= "#12b7de";
         leftBtn.style.zIndex="999";
         rightBtn.style.zIndex="999";
+    }*/
+
+
+    //左右过渡
+    let index=0;
+    let next=0;
+    leftBtn.onclick=function(){
+        if(!flag){
+            return;
+        }
+        flag=false;
+        next--;
+        if(next<0){
+            next=turnsLi.length-1;
+        }
+        turnsLi[next].style.left=-width+"px";
+        animate(turnsLi[index],{left:width},500,);
+        radioLi[index].classList.remove("hot");
+        animate(turnsLi[next],{left:0},500,function () {
+            flag=true;
+        });
+        radioLi[next].classList.add("hot");
+        index = next;
     }
+    rightBtn.onclick=function(){
+        if(!flag){
+            return;
+        }
+        flag=false;
+        next++;
+        if(next>=turnsLi.length){
+            next=0;
+        }
+        turnsLi[next].style.left=width+"px";
+        animate(turnsLi[index],{left:-width},500,);
+        radioLi[index].classList.remove("hot");
+        animate(turnsLi[next],{left:0},500,function () {
+            flag=true;
+        });
+        radioLi[next].classList.add("hot");
+        index = next;
+    }
+
+    for(let i=0;i<radioLi.length;i++){
+        radioLi[i].onclick = function () {
+            if (index === i){
+                return;
+            }
+            if(!flag){
+                return;
+            }
+            flag=false;
+            next=i;
+            if (next>index) {
+                turnsLi[next].style.left=width+"px";
+                animate(turnsLi[index],{left:-width},500,);
+                radioLi[index].classList.remove("hot");
+                animate(turnsLi[next],{left:0},500,function () {
+                    flag=true;
+                });
+                radioLi[next].classList.add("hot");
+                index = next;
+            }
+            else if(next<index){
+                turnsLi[next].style.left=-width+"px";
+                animate(turnsLi[index],{left:width},500,);
+                radioLi[index].classList.remove("hot");
+                animate(turnsLi[next],{left:0},500,function () {
+                    flag=true;
+                });
+                radioLi[next].classList.add("hot");
+                index = next;
+            }
+        }
+    }
+
     let t=setInterval(rightBtn.onclick,2000);
     photoLeft.onmouseenter=function(){
         clearInterval(t);
@@ -100,5 +181,24 @@ window.onload = function () {
         }
     })
 
+
+    let viewHeight = innerHeight;
+    let images = document.querySelectorAll(".lazy-onload");
+    let positionArry = [];
+    images.forEach(function (ele) {
+        let parent = ele.offsetParent;
+        positionArry.push(parent.offsetTop + ele.offsetTop)
+    })
+
+    window.onscroll = function () {
+        let scrolltop = document.documentElement.scrollTop;
+        for(let i=0;i<positionArry.length;i++){
+            if (scrolltop + viewHeight > positionArry[i]+50){
+                if (!images.src){
+                    images[i].src = images[i].getAttribute("aa");
+                }
+            }
+        }
+    }
 
 }
